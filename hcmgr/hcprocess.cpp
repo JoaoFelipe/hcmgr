d79b77ad84f3b42d52eb5a67c61c6589bff78191
JoaoFelipe <joaofelipenp@gmail.com>
2012-11-03 15:46:51 -0500

    Storing HornClauses

--- hcmgr/hcprocess.cpp
+++ hcmgr/hcprocess.cpp
@@ -12,24 +12,19 @@
 using namespace std;
 
 void process(string& filename){
+	vector<HornClause> horn_clauses;
 	SymbolTable table;
 	Parser parser;
 	parser.open(filename);
 	if (parser.is_open()) {
 		int i = 0;
 		while (!parser.eof()) {
-			//string current;
-			//parser.read_current(current);
-			//cout << i++ << ": " << current << endl;
-			//parser.next();
-
 			HornClause * hc = parser.parse_horn_clause();
 			if (hc == 0) {
 				parser.next_line();
-				//cout << "Fail" << endl;
 			} else {
 				hc->fill_symbol_table(table);
-				//cout << endl;
+				horn_clauses.push_back(*hc);
 			}
 
 			delete hc;
