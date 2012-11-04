634f8e4913f46f489461236dd5dd822d31f68715
amandapriscilla <amandafederal@gmail.com>
2012-11-03 23:27:40 -0500

    Update hcmgr/VariableEntry.h
    VariableEntry Comments

--- hcmgr/VariableEntry.h
+++ hcmgr/VariableEntry.h
@@ -1,3 +1,13 @@
+
+/**
+	VariableEntry.h
+	Class name: VariableEntry
+	Written by: Amanda Silva and João Pimentel
+	For:	    CSE 425 Lab 2
+	Purpose:    Declaration of the VariableEntry to represent Variables in the SymbolTable
+*/
+
+
 #ifndef VARIABLE_ENTRY_H
 #define VARIABLE_ENTRY_H
 
@@ -10,15 +20,43 @@ using namespace std;
 
 class VariableEntry : public SymbolEntry {
 public:
+
+//  @brief VariableEntry(const string & l) - VariableEntry class Constructor using a label string reference as parameter 
+//  @param l - reference to string value as label	
 	VariableEntry(const string & l);
+
+//  @brief VariableEntry(const VariableEntry & other) - VariableEntry class Copy Constructor 
+//  @param other - reference to another VariableEntry
 	VariableEntry(const VariableEntry & other);
+
+//  @brief clone()  - Returns the clone of the VariableEntry
+//  @return SymbolTableEntry
 	virtual SymbolTableEntry * clone() const;
+
+//  @brief operator==(const VariableEntry & other)- Compares the current VariableEntry to another VariableEntry 
+//  @param other - references to another VariableEntry
+//  @return bool - returns true if they hava same label
 	bool operator==(const VariableEntry & other) const;
+
+//  @brief type()  - Returns the name of the VariableEntry type
+//  @return string - name of the VariableEntry type 	
 	virtual string type() const;
+
+//  @brief text()  - Returns VariableEntry label
+//  @return string
 	virtual string text() const;
-	virtual bool is_variable() const;
+
+//  @brief is_constant()  - Returns true if the entry is constant
+//  @return bool
 	virtual bool is_constant() const;
+
+//  @brief is_variable()  - Returns true if the entry is variable
+//  @return bool
+	virtual bool is_variable() const;
+
+
 private:
+// Label of the VariableEntry
 	string _label;
 };
 
