30a17850ef6f8640a6e041cca9976120f609f233
amandapriscilla <amandafederal@gmail.com>
2012-11-03 22:09:28 -0500

    Update hcmgr/Symbol.h
    Symbol Comments

--- hcmgr/Symbol.h
+++ hcmgr/Symbol.h
@@ -1,3 +1,12 @@
+/**
+	Symbol.h
+	Class name: Symbol 
+	Written by: Amanda Silva and João Pimentel
+	For:	    CSE 425 Lab 2
+	Purpose:    Declaration of the Symbol  for the grammar
+				symbol -> LABEL | NUMBER
+*/
+
 #ifndef SYMBOL_H
 #define SYMBOL_H
 
@@ -11,18 +20,36 @@ using namespace std;
 class Symbol
 {
 private:
-    string _value;
+// Value of the Symbol
+	string _value;
 public:
+
+//  @brief Symbol(const string& v) - Symbol class Constructor using a value reference as parameter 
+//  @param v - reference to string value	
     Symbol(const string& v);
+	
+//  @brief Symbol(const Symbol& s) - Symbol class Copy Constructor 
+//  @param s - reference to another Symbol		
 	Symbol(const Symbol& s);
- 
+
+//  @brief value(const string& v) - Setter to the Symbol value 
+//  @param v - reference to string value	
     void value(const string& v);
+
+//  @brief value() - Getter to the Symbol value 
+//  @return string 	
     string value() const;
 
+//  @brief text() - Returns a text with the type and value of the Symbol 
+//  @return string 
 	string text() const;
 
+//  @brief print(ostream & output)  - Prints the Symbol according to the grammar syntax
+//  @param output - ostream
 	void print(ostream & = cout) const; 
 
+//  @brief Symbol::convertToSymbolTableEntry()  - Converts the Symbol to a SymbolTableEntry
+//  @return SymbolTableEntry
 	SymbolTableEntry * convertToSymbolTableEntry() const;
 };
  
