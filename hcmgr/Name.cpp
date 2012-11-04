d80bde4fc0fa3f267f85d4252344119c3a5efdb3
amandapriscilla <amandafederal@gmail.com>
2012-11-03 20:02:53 -0500

    Update hcmgr/Name.cpp
    Comments

--- hcmgr/Name.cpp
+++ hcmgr/Name.cpp
@@ -1,17 +1,37 @@
+/**
+	Name.cpp
+	Class name: Name 
+	Written by: Amanda Silva and João Pimentel
+	For:	    CSE 425 Lab 2
+	Purpose:    Implementation of the Name for the grammar
+			    name -> LABEL
+*/
+
 #include "stdafx.h"
 #include "Name.h"
 
+//  @brief Name(const string& v) - Name class Constructor using a value string parameter
+//  @param v - reference to a string value
 Name::Name(const string& v): _value(v) {}
+
+//  @brief Name(const Name& n) - Name class Copy Constructor
+//  @param n - reference to a name
 Name::Name(const Name& n) : _value(n._value) {}
 
+//  @brief value(const string& v) - Setter method to the Name value
+//  @param v - reference to a string value
 void Name::value(const string& v) {
 	_value = v;
 }
 
+//  @brief value() - Getter method to the Name value
+//  @return string
 string Name::value() const {
 	return _value;
 }
 
+//  @brief print(ostream & output)  - Prints the Name value according to the grammar syntax
+//  @param output - ostream
 void Name::print(ostream & output) const {
 	output << _value;
 }
