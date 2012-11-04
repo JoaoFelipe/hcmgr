bfbdf81e3e1a7edad1f9763000996ffd1a95e3ee
amandapriscilla <amandafederal@gmail.com>
2012-11-03 23:42:05 -0500

    Update hcmgr/string_utils.h
    string_utils.h comments

--- hcmgr/string_utils.h
+++ hcmgr/string_utils.h
@@ -1,3 +1,10 @@
+/**
+  string_utils.h
+	Written by: Amanda Silva and João Pimentel
+	For:	    CSE 425 Lab 2
+	Purpose:    Auxiliar Functions
+*/
+
 #ifndef STRING_UTILS_H
 #define STRING_UTILS_H 
 
@@ -6,9 +13,20 @@
 
 using namespace std; 
 
+//  @brief is_lower (const char c)  - Checks if the char is lowercase and alphanumeric
+//  @return int - returns zero if it is false
 int is_lower (const char c);
+
+//  @brief is_upper (const char c)  - Checks if the char is uppercase and alphanumeric
+//  @return int - returns zero if it is false
 int is_upper (const char c);
+
+//  @brief is_number (const char c)  - Checks if the char is numeric
+//  @return int - returns zero if it is false
 int is_number (const char c);
+
+//  @brief check_string(const string & str, int (*checker)(const char)) - Applies a checker function to each character of the string
+//  @return bool 
 bool check_string(const string & str, int (*checker)(const char));
 
 #endif
