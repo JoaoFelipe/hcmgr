df881f62f42f569d265f98feaa2457872e5450b6
amandapriscilla <amandafederal@gmail.com>
2012-11-03 22:37:40 -0500

    Update hcmgr/SymbolEntry.h
    SymbolEntry Comments

--- hcmgr/SymbolEntry.h
+++ hcmgr/SymbolEntry.h
@@ -1,3 +1,10 @@
+/**
+	SymbolEntry.h
+	Class name: SymbolEntry 
+	Written by: Amanda Silva and João Pimentel
+	For:	    CSE 425 Lab 2
+	Purpose:    Declaration of the SymbolEntry to represent a Symbol in the SymbolTable
+*/
 #ifndef SYMBOL_ENTRY_H
 #define SYMBOL_ENTRY_H
 
@@ -10,9 +17,21 @@ using namespace std;
 
 class SymbolEntry : public SymbolTableEntry {
 public:
+
+//  @brief clone()  - Returns the clone of the SymbolTableEntry
+//  @return SymbolTableEntry
 	virtual SymbolTableEntry * clone() const = 0;
+
+//  @brief is_predicate()  - Returns true if a SymbolTableEntry is a Predicate
+//  @return bool 
 	virtual bool is_predicate() const;
+
+//  @brief matches(SymbolTableEntry * other, SubstitutionList & substitution_list)  - Returns true if the current SymbolTableEntry matches with other SymbolTableEntry
+//  @return bool
 	virtual bool SymbolEntry::matches(SymbolTableEntry * other, SubstitutionList & substitution_list) const;
+	
+//  @brief unification(SubstitutionList & substitution_list)  - Returns the modified symbols with the substitutions after the unification 
+//  @return string - symbols after substitutions 			
 	virtual string SymbolEntry::unification(SubstitutionList & substitution_list) const;
 };
 
