2e5805ccccdb6ea1a091c1073787131dbf4a3df0
amandapriscilla <amandafederal@gmail.com>
2012-11-03 22:27:18 -0500

    Update hcmgr/SymbolEntry.cpp
    SymbolEntry Comments

--- hcmgr/SymbolEntry.cpp
+++ hcmgr/SymbolEntry.cpp
@@ -1,12 +1,23 @@
+/**
+	SymbolEntry.cpp
+	Class name: SymbolEntry 
+	Written by: Amanda Silva and João Pimentel
+	For:	    CSE 425 Lab 2
+	Purpose:    Implementation of the SymbolEntry to represent a Symbol in the SymbolTable
+*/
+
 #include "stdafx.h"
 #include "SymbolEntry.h"
 #include "const.h"
 
+//  @brief is_predicate()  - Returns true if a SymbolTableEntry is a Predicate
+//  @return bool 
 bool SymbolEntry::is_predicate() const {
 	return false;
 }
 
-
+//  @brief matches(SymbolTableEntry * other, SubstitutionList & substitution_list)  - Returns true if the current SymbolTableEntry matches with other SymbolTableEntry
+//  @return bool 		
 bool SymbolEntry::matches(SymbolTableEntry * other, SubstitutionList & substitution_list) const {
 	if (!other->is_variable() && !other->is_constant()) {
 		return false;
@@ -25,6 +36,8 @@ bool SymbolEntry::matches(SymbolTableEntry * other, SubstitutionList & substitut
 	return (ts == os);
 }
 
+//  @brief unification(SubstitutionList & substitution_list)  - Returns the modified symbols with the substitutions after the unification 
+//  @return string - symbols after substitutions 		
 string SymbolEntry::unification(SubstitutionList & substitution_list) const {
 	const SymbolTableEntry * super = dynamic_cast<const SymbolTableEntry *>(this);
 	SymbolTableEntry * ts = substitution_list.find(super);
